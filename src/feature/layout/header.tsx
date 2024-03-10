47ac779e30e4c17acfecce57e9dadb842bce4062
kinjo <kinjoyuji@gmail.com>
2024-03-11 01:15:05 +0900

    Update header styling to include a fixed position

--- src/feature/layout/header.tsx
+++ src/feature/layout/header.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 
 export default function Header() {
   return (
-    <header className="flex p-4 items-center place-content-between">
+    <header className="flex p-4 items-center place-content-between fixed top-0 left-0 right-0 z-50 bg-white dark:bg-black">
       <div className="flex items-center gap-4">
         <Image src={"/code-kitchen/leo.webp"} alt={"logo"} width={"36"} height={"36"}/>
         <div className='prose dark:prose-invert'>CodeKichen</div>
