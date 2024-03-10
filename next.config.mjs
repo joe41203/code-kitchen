718ee7d1252b73dce3a859777bc313ff76dcc293
kinjo <kinjoyuji@gmail.com>
2024-03-11 00:02:26 +0900

    Add output export and enable unoptimized images

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/code-kitchen",
-  assetPrefix: "/code-kitchen"
+  assetPrefix: "/code-kitchen",
+  output: "export",
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
