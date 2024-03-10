97afe3036e3bd34b5d6f3c0967ae8934e900d109
kinjo <kinjoyuji@gmail.com>
2024-03-11 01:25:24 +0900

    Update header layout with logo link and styling

--- src/feature/layout/header.tsx
+++ src/feature/layout/header.tsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React from 'react';
 import { ThemeToogle } from '@/theme/theme-toogle';
-import Image from "next/image"
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Header() {
   return (
     <header className="flex p-4 items-center place-content-between fixed top-0 left-0 right-0 z-50 bg-white dark:bg-black">
       <div className="flex items-center gap-4">
-        <Image src={"/code-kitchen/leo.webp"} alt={"logo"} width={"36"} height={"36"}/>
-        <div className='prose dark:prose-invert'>CodeKichen</div>
+        <Link href={'/articles'}>
+          <Image src={'/code-kitchen/leo.webp'} alt={'logo'} width={'36'} height={'36'} />
+        </Link>
+        <Link href={'/articles'}>
+          <div className="prose dark:prose-invert">CodeKichen</div>
+        </Link>
       </div>
-      <ThemeToogle/>
+      <ThemeToogle />
     </header>
-  )
+  );
 }
