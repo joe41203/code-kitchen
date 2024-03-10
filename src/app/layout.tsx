a8f9a992e6381cfc46406a971cf21de37f2d846b
kinjo <kinjoyuji@gmail.com>
2024-03-10 19:54:06 +0900

    Add Prettier configuration and update lint script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,16 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { ThemeProvider } from "@/theme/theme-provider";
-import clsx from "clsx";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { ThemeProvider } from '@/theme/theme-provider';
+import clsx from 'clsx';
 import Header from '@/feature/layout/header';
-import Footer from "@/feature/layout/footer";
+import Footer from '@/feature/layout/footer';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Code Kitchen",
-  description: "Code Kitchen",
+  title: 'Code Kitchen',
+  description: 'Code Kitchen',
 };
 
 export default function RootLayout({
@@ -23,9 +23,9 @@ export default function RootLayout({
       <body className={clsx(inter.className, 'h-full bg-background')}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           <div className="h-full flex flex-col">
-            <Header/>
+            <Header />
             <div className="flex-1 max-w-lg m-auto py-20 w-full">{children}</div>
-            <Footer/>
+            <Footer />
           </div>
         </ThemeProvider>
       </body>
