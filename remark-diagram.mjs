a5d51e9e9133e7742840148a557a28051f5f6a70
kinjo <kinjoyuji@gmail.com>
2024-03-15 08:52:59 +0900

    Add mermaid diagram styling and tmp dir creation

--- remark-diagram.mjs
+++ remark-diagram.mjs
@@ -1,5 +1,5 @@
 import { execSync } from "child_process";
-import { readFileSync, unlinkSync, writeFileSync } from "fs";
+import { existsSync, mkdirSync, readFileSync, unlinkSync, writeFileSync } from "fs";
 import path from "path";
 import { visit } from "unist-util-visit";
 
@@ -7,18 +7,24 @@ export function remarkDiagram() {
   return function (tree, { __ }) {
     visit(tree, "code", (node) => {
       if (node.lang == "mermaid") {
-        const tempInputFile = path.join(process.cwd(), "temp-mermaid-input.mmd");
-        const tempOutputFile = path.join(process.cwd(), "temp-mermaid-output.svg");
+        const tempDir = path.join(process.cwd(), "tmp");
+        const tempInputFile = path.join(tempDir, "mermaid-input.mmd");
+        const tempOutputFile = path.join(tempDir, "mermaid-output.svg");
+
+        if (!existsSync(tempDir)) {
+          mkdirSync(tempDir);
+        }
 
         writeFileSync(tempInputFile, node.value);
 
         try {
-          execSync(`npx mmdc -i ${tempInputFile} -o ${tempOutputFile}`);
+          execSync(`npx mmdc -i ${tempInputFile} -o ${tempOutputFile} -c mermaid.config.json`);
 
           const svgContent = readFileSync(tempOutputFile, "utf8");
+          const styledSvgContent = `<div style="width: 100%; overflow: auto;">${svgContent}</div>`;
 
           node.type = "html";
-          node.value = svgContent;
+          node.value = styledSvgContent;
 
         } catch (error) {
           console.error("Error processing Mermaid diagram:", error);
