1f7bbdb3e216ed63dceafd5141be1e4a322c723c
kinjo <kinjoyuji@gmail.com>
2024-03-14 20:01:51 +0900

    Replace into astro

--- src/feature/layout/footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-    <div className='flex p-4 justify-center' >
-      <small className="prose dark:prose-invert">Copyright © 2024 Code Kitchen</small>
-    </div>
-  )
-}
