2017ad18ac38439b0fce6d2837935aede727c0e1
kinjo <kinjoyuji@gmail.com>
2024-03-14 21:58:55 +0900

    Add robots.txt integration for SEO improvement

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://code-kitchen.pages.dev',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), robotsTxt()],
   base: '/',
   markdown: {
-    syntaxHighlight: 'prism',
+    syntaxHighlight: 'prism'
   }
 });
