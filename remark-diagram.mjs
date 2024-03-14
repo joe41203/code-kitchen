62f52ab9254d42c81ccfaf303d46c070ac990fb9
kinjo <kinjoyuji@gmail.com>
2024-03-15 02:04:21 +0900

    Add @mermaid-js/mermaid-cli package and update remark-diagram

--- remark-diagram.mjs
+++ remark-diagram.mjs
@@ -1,11 +1,33 @@
+import { execSync } from "child_process";
+import { readFileSync, unlinkSync, writeFileSync } from "fs";
+import path from "path";
 import { visit } from "unist-util-visit";
 
 export function remarkDiagram() {
-  return function (tree, { _ }) {
+  return function (tree, { __ }) {
     visit(tree, "code", (node) => {
       if (node.lang == "mermaid") {
-        node.type = "html";
-        node.value = '<div class ="' + node.lang + '">' + node.value + "</div>";
+        const tempInputFile = path.join(process.cwd(), "temp-mermaid-input.mmd");
+        const tempOutputFile = path.join(process.cwd(), "temp-mermaid-output.svg");
+
+        writeFileSync(tempInputFile, node.value);
+
+        try {
+          execSync(`npx mmdc -i ${tempInputFile} -o ${tempOutputFile}`);
+
+          const svgContent = readFileSync(tempOutputFile, "utf8");
+
+          node.type = "html";
+          node.value = svgContent;
+
+        } catch (error) {
+          console.error("Error processing Mermaid diagram:", error);
+          node.value = `<pre>Error processing Mermaid diagram:\n${error.message}</pre>`;
+        } finally {
+          // 一時ファイルを削除する
+          unlinkSync(tempInputFile);
+          unlinkSync(tempOutputFile);
+        }
       }
     });
   };
