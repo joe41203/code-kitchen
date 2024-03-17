fcb2ec08579a20de641786fef43259aecdbe6923
kinjo <kinjoyuji@gmail.com>
2024-03-17 17:09:29 +0900

    Remove astro-compress and update integrations in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,16 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
 import { defineConfig } from 'astro/config';
 import { remarkMermaid } from "./remark-mermaid.mjs";
-import robotsTxt from "astro-robots-txt";
-import compress from "astro-compress";
 
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://code-kitchen.pages.dev',
-  integrations: [mdx(), sitemap(), tailwind(), robotsTxt(), compress(), partytown()],
+  integrations: [mdx(), sitemap(), tailwind(), robotsTxt(), partytown()],
   base: '/',
   trailingSlash: "ignore",
   markdown: {
