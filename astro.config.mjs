789cb33acdc41f696631f9836931c2de93d01438
kinjo <kinjoyuji@gmail.com>
2024-03-17 21:15:21 +0900

    Add icon data to astro config- Add iconData to webmanifest icons- Add icons.json import- Add icons.json with various sizes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
 import webmanifest from "astro-webmanifest";
 import { defineConfig } from "astro/config";
 import serviceWorker from "astrojs-service-worker";
+import iconData from "./public/icons/icons.json";
 import { remarkMermaid } from "./remark-mermaid.mjs";
-import partytown from "@astrojs/partytown";
 
 import compress from "astro-compress";
 
@@ -19,13 +20,13 @@ export default defineConfig({
     }
   }), serviceWorker(), webmanifest({
     name: 'CodeKitchen',
-    icon: '/public/images/profile.webp',
     short_name: 'CodeKitchen',
     description: 'CodeKitchen',
     start_url: 'https://code-kitchen.tech/',
     theme_color: '#f4f4ff',
     background_color: '#f4f4ff',
-    display: "standalone"
+    display: "standalone",
+    icons: iconData["icons"]
   }), compress()],
   base: "/",
   trailingSlash: "ignore",
