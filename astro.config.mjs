688763e23444e72af994b572f0d52329247c4e9e
kinjo <kinjoyuji@gmail.com>
2024-03-17 17:44:40 +0900

    Add Google Tag Manager to layout and base components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,34 @@
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import { remarkMermaid } from "./remark-mermaid.mjs";
 
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://code-kitchen.pages.dev',
-  integrations: [mdx(), sitemap(), tailwind(), robotsTxt(), partytown()],
-  base: '/',
+  site: "https://code-kitchen.pages.dev",
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    robotsTxt(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
+  base: "/",
   trailingSlash: "ignore",
   markdown: {
     remarkPlugins: [remarkMermaid],
     shikiConfig: {
-      theme: 'github-dark',
+      theme: "github-dark",
       langs: [],
-      wrap: false
-    }
-  }
+      wrap: false,
+    },
+  },
 });
