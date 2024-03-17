44fe06cb550bb202d086db98467c09dbeda0eed5
kinjo <kinjoyuji@gmail.com>
2024-03-17 21:07:15 +0900

    Add compress integration to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,35 +6,27 @@ import webmanifest from "astro-webmanifest";
 import { defineConfig } from "astro/config";
 import serviceWorker from "astrojs-service-worker";
 import { remarkMermaid } from "./remark-mermaid.mjs";
-
 import partytown from "@astrojs/partytown";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://code-kitchen.tech",
-  integrations: [
-    mdx(),
-    sitemap(),
-    tailwind(),
-    robotsTxt(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-    serviceWorker(),
-    webmanifest({
-      name: 'CodeKitchen',
-      icon: '/public/images/profile.webp',
-      short_name: 'CodeKitchen',
-      description: 'CodeKitchen',
-      start_url: 'https://code-kitchen.tech/',
-      theme_color: '#f4f4ff',
-      background_color: '#f4f4ff',
-      display: "standalone",
-    })
-
-  ],
+  integrations: [mdx(), sitemap(), tailwind(), robotsTxt(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), serviceWorker(), webmanifest({
+    name: 'CodeKitchen',
+    icon: '/public/images/profile.webp',
+    short_name: 'CodeKitchen',
+    description: 'CodeKitchen',
+    start_url: 'https://code-kitchen.tech/',
+    theme_color: '#f4f4ff',
+    background_color: '#f4f4ff',
+    display: "standalone"
+  }), compress()],
   base: "/",
   trailingSlash: "ignore",
   markdown: {
@@ -42,7 +34,7 @@ export default defineConfig({
     shikiConfig: {
       theme: "github-dark",
       langs: [],
-      wrap: false,
-    },
-  },
+      wrap: false
+    }
+  }
 });
