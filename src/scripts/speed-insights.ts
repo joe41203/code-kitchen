13b0deef2373cd74c23a2002ff53b9c0e0da3974
kinjo <kinjoyuji@gmail.com>
2024-03-22 19:08:38 +0900

    Update sleep time to 200 milliseconds

--- src/scripts/speed-insights.ts
+++ src/scripts/speed-insights.ts
@@ -261,7 +261,7 @@ const main = async (): Promise<void> => {
       }
 
       // スリープ処理を追加 (例: 1秒待機)
-      await new Promise((resolve) => setTimeout(resolve, 500));
+      await new Promise((resolve) => setTimeout(resolve, 200));
     }
   }
 
